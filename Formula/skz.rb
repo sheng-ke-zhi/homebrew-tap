f3564a5fee2180a6b1fe71c210dc4110426cf22d
chenyue <root.schwarz.chan@gmail.com>
2026-07-27 16:06:08 +0800

    skz v0.1.3

--- Formula/skz.rb
+++ Formula/skz.rb
@@ -6,22 +6,22 @@ class Skz < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/sheng-ke-zhi/skz-quant-cli/releases/download/v0.1.3/skz-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/sheng-ke-zhi/skz-quant-cli-releases/releases/download/v0.1.3/skz-aarch64-apple-darwin.tar.gz"
       sha256 "c0a45ed7b2bd84233c42b33a0ddc705587ca57a8e706e6947225c9f86a4c9620"
     end
     on_intel do
-      url "https://github.com/sheng-ke-zhi/skz-quant-cli/releases/download/v0.1.3/skz-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/sheng-ke-zhi/skz-quant-cli-releases/releases/download/v0.1.3/skz-x86_64-apple-darwin.tar.gz"
       sha256 "b9958c9ff630a56bf668541435a77030a74570011eff3a74874bcbb3ef1219d7"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/sheng-ke-zhi/skz-quant-cli/releases/download/v0.1.3/skz-aarch64-unknown-linux-musl.tar.gz"
+      url "https://github.com/sheng-ke-zhi/skz-quant-cli-releases/releases/download/v0.1.3/skz-aarch64-unknown-linux-musl.tar.gz"
       sha256 "cd5f1690f58816ea39be293b93ffdb07708c5ccfca4b9cdedc91918bc4f18abf"
     end
     on_intel do
-      url "https://github.com/sheng-ke-zhi/skz-quant-cli/releases/download/v0.1.3/skz-x86_64-unknown-linux-musl.tar.gz"
+      url "https://github.com/sheng-ke-zhi/skz-quant-cli-releases/releases/download/v0.1.3/skz-x86_64-unknown-linux-musl.tar.gz"
       sha256 "8d38d3f5fa9ecc0490e37cdc1683c0fcb69769a13473ba4b76c56c90da4694d4"
     end
   end
